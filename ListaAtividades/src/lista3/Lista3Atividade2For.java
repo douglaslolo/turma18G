4e124a697051279d9d5aaa0601cfca64aa3c876e
Douglas Silva <douglas-pereira@outlook.com>
2021-03-07 14:22:30 -0300

    add/corrigindo comentários

--- ListaAtividades/src/lista3/Lista3Atividade2For.java
+++ ListaAtividades/src/lista3/Lista3Atividade2For.java
@@ -1,7 +1,8 @@
 package lista3;
 	
-	/*
-	
+	/*2. Desenvolver um sistema que efetue a soma de todos os números 
+	ímpares que são  múltiplos de três e que se encontram no 
+	conjunto dos números de 1 até 500.
 	*/
 
 public class Lista3Atividade2For {
--- ListaAtividades/src/lista3/Lista3Atividade2While.java
+++ ListaAtividades/src/lista3/Lista3Atividade2While.java
@@ -2,7 +2,7 @@ package lista3;
 
 import java.text.DecimalFormat;
 import java.util.Scanner;
-	/*2- Obtenha um número digitado pelo usuário e repita a operação de multiplicar 
+	/*2. Obtenha um número digitado pelo usuário e repita a operação de multiplicar 
 	ele por três  (imprimindo o novo valor) até que ele seja maior do que 100. Ex.: 
 	se o usuário digita 5,  deveremos observar na tela a seguinte sequência: 5 15 45 135.
 	*/
