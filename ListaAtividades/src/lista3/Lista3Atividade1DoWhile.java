d32ca6e2ce5795a2c331b77d7105a3233439f409
Douglas Silva <douglas-pereira@outlook.com>
2021-03-07 13:47:24 -0300

    Lista de atividades 2 em JAVA

---
+++ ListaAtividades/src/lista3/Lista3Atividade1DoWhile.java
@@ -0,0 +1,22 @@
+package lista3;
+
+	/*1. Faça um programa que mostre uma contagem na tela de 233 a 456, só que contando de 
+	3 em 3 quando estiver entre 300 e 400 e de 5 em 5 quando não estiver.
+	*/
+
+public class Lista3Atividade1DoWhile {
+	public static void main(String[] args) {
+		int contagem = 233;
+		
+		do {
+			if(contagem >= 300) {
+				contagem = (contagem + 5);				
+			}
+			else {
+				contagem = (contagem + 3);
+			}
+			System.out.println(contagem + "\n");
+		}
+		while(contagem < 400);		
+	}
+}
---
+++ ListaAtividades/src/lista3/Lista3Atividade2For.java
@@ -0,0 +1,21 @@
+package lista3;
+	
+	/*
+	
+	*/
+
+public class Lista3Atividade2For {
+	
+	public static void main(String[] args) {
+		int soma=0, cont =0;
+		
+		for(int i=1; i<=500; i+=2) {
+			if(i % 3 == 0) {
+				cont = (cont + 1);
+				soma = (soma + i);
+			}
+		}
+		System.out.println("A soma dos ímpares múltiplos de três, contando de 1 até 500 é de: " + soma + "\n");
+		System.out.println("E a quantidade de números contados foi de: " + cont);
+	}
+}
